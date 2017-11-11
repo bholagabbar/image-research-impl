19ddc53f3bf3286d071ca757cbe886e3167d68c3
bholagabbar <dev.shreyans96@gmail.com>
2017-11-11 22:46:08 +0530

    Added plotting file

---
+++ plot_average_graphs.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+plt.plot([0.1, 0.2, 0.4 , 0.5, 0.6, 0.8], [0.70209,0.7019,0.70151,0.70138,0.701265,0.700975], 'ro')
+plt.xlabel('Message Size*P')
+plt.ylabel('SSIM')
+plt.show()
